698d0873d2dd1685043eaba665aeebea18118f9b
sowmiya <sowmiya.cs17@bitsathy.ac.in>
2020-07-14 21:17:27 +0530

    sample 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import CourseList from './component/CoursesList'
+import './App.css';
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <CourseList />
       </div>
     )
   }
